74ae97250064b5b4a256b1eb931fa494a0432a7d
SundousKanaan <94317411+SundousKanaan@users.noreply.github.com>
2025-07-30 13:28:02 +0200

    feat: add the firebase to the android app

--- android/app/src/main/java/com/capisoft/logo/MainActivity.java
+++ android/app/src/main/java/com/capisoft/logo/MainActivity.java
@@ -1,5 +1,16 @@
 package com.capisoft.logo;
-
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+import android.os.Bundle;
+import com.getcapacitor.Plugin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // No manual plugin registration typically needed with Capacitor 4/5+
+  }
+}
